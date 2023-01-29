efc44b7f0ef126506be8117f9fb25f171fc1154f
minjj0905 <minjj0905@daum.net>
2023-01-29 23:44:58 +0900

    chore: emotion setting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+/** @jsxImportSource @emotion/react */
+
 import React from 'react';
+import { css } from '@emotion/react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div css={css({ color: 'red' })}>Home</div>;
 };
 
 export default Home;
